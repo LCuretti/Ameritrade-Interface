129589c7f8540b2c5a7daced707043f0c8375b77
Luis Curetti <curetti_luis@hotmail.com>
2019-11-21 09:41:23 -0300

    Add files via upload

---
+++ Ameritrade_cfg_emptyexample.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 11:23:06 2019
+
+@author: LC
+"""
+
+# you have an explaining video on how to register your API at Ameritrade:
+# https://www.youtube.com/watch?v=qJ94sSyPGBw
+
+client_id = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'  # client ID you have to get when create an API in https://developer.tdameritrade.com
+redirect_uri = 'http://localhost/test' #This is and example of what you could chosse when register your API at Ameritrade developer page 
+password = '***************'  #your account password (in case you want to load from here. You may enter on the Authetication API from command line or at Webpage)
+account_id = '*********'    #your account ID
+user = '********'  # Your user name (in case you want to load from here. You may enter on the Authetication API from command line or at Webpage)
