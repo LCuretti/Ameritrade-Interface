5c662c7b7fcae254164adf224dd3413bb4dab9b6
Luis Curetti <curetti_luis@hotmail.com>
2021-04-10 08:58:29 -0300

    Add files via upload
    Credentials example file for new object feature system.

---
+++ TD/Ameritrade_cfg_obj_emptyexample.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 23 11:23:06 2019
+
+@author: Luis
+"""
+
+class TDConfig():
+    def __init__(self):
+        self.account_id = '*********' #your account ID
+        self.redirect_uri = 'http://localhost/test' #This is and example of what you could chosse when register your API at Ameritrade developer page
+        self.client_id = '********************************' # client ID you have to get when create an API in https://developer.tdameritrade.com
+
